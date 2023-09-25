d973593e3568cba382c640896d5b28b70a54b4ed
Liam Gorman <lrgorman1@cps.edu>
2023-09-25 09:50:59 -0500

    the rest of a2

--- a2.py
+++ a2.py
@@ -28,29 +28,50 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
 
         # 1) if we reached the end of the pattern but not source
         if pind == len(pattern) and sind != len(source):
-            
+            return None
+
         # 2) if the current thing in the pattern is a %
         if pattern(pind) == "%":
+            if pind == (len(pattern)-1):
+                result.append(" ".join(source[sind:]))
+                return result
+        else:
+            accum = ""
+            pind += 1 
+            while pattern[pind] != source[sind]:
+                accum += source[sind] + " "
+                sind += 1
+
+            result.append(accum.strip)
 
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
 
         # 3) if we reached the end of the source but not the pattern
-        if sind == len(source):
+        elif sind == len(source) and pind != len(pattern):
+            return None
+
 
         # 4) if the current thing in the pattern is an _
-        if pattern[pind] == "_":
+        elif pattern[pind] == "_":
+            result.append(source[sind])
+            pind += 1
+            sind += 1
+
        
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-        if pattern[pind] == source[sind]:
+        elif pattern[pind] == source[sind]:
             pind += 1
             sind += 1
 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
+        else:
+            return None 
+
 
     return result
 
